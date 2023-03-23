efac97b273b43eb0b6f74b03d87562c91c542d3a
Heroshi Hakatama <127243300+W8gReZW@users.noreply.github.com>
2023-03-23 19:52:07 -0400

    Update background.js

--- background.js
+++ background.js
@@ -6,3 +6,29 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
   });
   
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("request", request)
+    if (request.type === 'getSessionCookie') {
+      chrome.cookies.get({ url: request.url, name: 'sessionid' }, (cookie) => {
+        sendResponse(cookie);
+      });
+      return true; // This is required to use sendResponse asynchronously.
+    }
+    // ... (handle other message types if needed)
+  });
+  
+
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'getCSRFToken') {
+      chrome.cookies.get({ url: message.url, name: 'csrftoken' }, (cookie) => {
+        if (cookie) {
+          sendResponse(cookie.value);
+        } else {
+          sendResponse(null);
+        }
+      });
+      return true; // Return true to indicate that sendResponse will be called asynchronously
+    }
+  });
