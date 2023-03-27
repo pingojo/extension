316571aed8c600e8749a39affe8a3609e8b83d04
Heroshi Hakatama <127243300+W8gReZW@users.noreply.github.com>
2023-03-27 00:08:45 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,34 +1,52 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'addContact') {
-      const { name, email } = request.contact;
-      console.log('Contact added:', name, email);
-      sendResponse({ success: true });
-    }
-  });
-  
-
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("request", request)
-    if (request.type === 'getSessionCookie') {
-      chrome.cookies.get({ url: request.url, name: 'sessionid' }, (cookie) => {
-        sendResponse(cookie);
-      });
-      return true; // This is required to use sendResponse asynchronously.
-    }
-    // ... (handle other message types if needed)
-  });
-  
+  if (request.type === 'addContact') {
+    const {
+      name,
+      email
+    } = request.contact;
+    sendResponse({
+      success: true
+    });
+  }
+});
 
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'getCSRFToken') {
-      chrome.cookies.get({ url: message.url, name: 'csrftoken' }, (cookie) => {
-        if (cookie) {
-          sendResponse(cookie.value);
-        } else {
-          sendResponse(null);
-        }
-      });
-      return true; // Return true to indicate that sendResponse will be called asynchronously
-    }
-  });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'getSessionCookie') {
+    chrome.cookies.get({
+      url: request.url,
+      name: 'sessionid'
+    }, (cookie) => {
+      sendResponse(cookie);
+    });
+    return true;
+  }
+});
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'getCSRFToken') {
+    chrome.cookies.get({
+      url: message.url,
+      name: 'csrftoken'
+    }, (cookie) => {
+      if (cookie) {
+        sendResponse(cookie.value);
+      } else {
+        sendResponse(null);
+      }
+    });
+    return true; // Return true to indicate that sendResponse will be called asynchronously
+  }
+});
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'getCurrentTab') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      sendResponse(currentTab);
+    });
+    return true;
+  }
+});
